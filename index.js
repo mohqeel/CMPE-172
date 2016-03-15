fb6a69654d87fb7b0f8463728be6795a804807cc
Ubuntu <ubuntu@ip-172-31-34-20.us-west-2.compute.internal>
2016-03-15 02:15:15 +0000

    Part 1-B

--- index.js
+++ index.js
@@ -5,11 +5,5 @@ fs.readFile('index.html', 'utf-8', function(err, data) {
         if(err) {
                 return console.log(err);
         }
-        console.log('Loaded the HTML page');
-        http.createServer(function(req, res) {
-                res.writeHeader(200, {"Content-Type": "text/html"});
-                res.write(html);
-                res.end();
-        }).listen(8080);
-        console.log('server started on port 8080');
+	return data;
 });
