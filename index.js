f808507c45318f6a26550ef93d48269d5c411379
Ubuntu <ubuntu@ip-172-31-34-20.us-west-2.compute.internal>
2016-03-15 02:03:16 +0000

    Part 1-B

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 
-var html = fs.readFileSync('index.html', 'utf-8');
-
-http.createServer(function(request, response) {
-	res.writeHeader(200, {"Content-Type": "text/html"});
-        res.write(html);
-	res.end();
-}).listen(8080);
-console.log('server started on port 8080');
+fs.readFile('index.html', 'utf-8', function(err, data) {
+        if(err) {
+                return console.log(err);
+        }
+        console.log('Loaded the HTML page');
+        http.createServer(function(req, res) {
+                res.writeHeader(200, {"Content-Type": "text/html"});
+                res.write(html);
+                res.end();
+        }).listen(8080);
+        console.log('server started on port 8080');
+});
