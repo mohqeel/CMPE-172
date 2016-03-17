d10f29c4a81c78961073dbcf24d076867b162303
Ubuntu <ubuntu@ip-172-31-34-20.us-west-2.compute.internal>
2016-03-17 21:31:06 +0000

    Testing

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var file = fs.readFileSync('index.html', 'utf-8');
 
-console.log(file);
-
 http.createServer(function (req, res) {
 	res.wtireHead(200);
 	res.end(file);
-}).listen(8080);
+}).listen(process.env.PORT || 5000);
