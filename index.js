2ac42803a9da294e47b4d45e729960ae3061c84f
Ubuntu <ubuntu@ip-172-31-34-20.us-west-2.compute.internal>
2016-03-17 21:38:49 +0000

    Part 1-A

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var http = require('http');
 var fs = require('fs');
+
 var file = fs.readFileSync('index.html', 'utf-8');
+console.log("Index file read");
 
 http.createServer(function (req, res) {
 	res.writeHead(200);
 	res.end(file);
 }).listen(process.env.PORT || 5000);
+
+if(typeof process.env.PORT !== 'undefined'){
+	console.log("Server started on port" + process.env.PORT);
+} else {
+	console.log("Server started on port 5000");
+}
