551ec1fd1022cffc3fe445d3c548ea37d1049def
Ubuntu <moha.gagg@gmail.com>
2016-03-18 03:24:31 +0000

    Part 2 - final

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var http = require('http');
 var fs = require('fs');
+var fileName = 'index.html'
 
-http.createServer( function (req, res) {
-	fs.readFile('index.html', function(err, data) {
-		if(err) {
-			res.writteHead(404);
-			res.end(JSON.stringify(err));
-			return;
-		}
-		res.writeHead(200);
-		res.end(data);
-	});
-}).listen(process.env.PORT || 5000);
+fs.exists(fileName, function(exists) {
+	if(exists) {
+		fs.stat(fileName, function(error, stats) {
+			fs.open(fileName, 'r', function(error, fd) {
+				var buffer = new Buffer(stats.size);
 
-if(typeof process.env.PORT !== 'undefined'){
-	console.log("Server started on port" + process.env.PORT);
-} else {
-	console.log("Server started on port 5000");
-}
+				fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+					var data = buffer.toString('utf-8', 0, buffer.length);
+
+					http.createServer(function(req, res) {
+						res.writeHead(200);
+						res.end(data);
+					}).listen(process.env.PORT || 5000);
+					
+					if(typeof process.env.PORT !== 'undefined'){
+						console.log("Server started on port" + process.env.PORT);
+					} else {
+						console.log("Server started on port 5000");
+					}
+					fs.close(fd);
+				});
+			});
+		});
+	}
+});
